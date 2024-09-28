df2e3aec1ff30036e6ddf2bb8b8181b75adf7a8b
Moaksh <moakshkakar@gmail.com>
2024-09-28 14:20:54 +0530

    build error fix

--- app/page.tsx
+++ app/page.tsx
@@ -415,7 +415,7 @@ const Home: React.FC = () => {
 
                         )}
                         <button
-                            onClick={handleSubmit}
+                            onClick={() => handleSubmit()}
                             disabled={currentStep >= steps.length}
                             className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
                         >
